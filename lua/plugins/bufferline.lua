c3d0baea7677a1354d13b72e55c8cb4a68565a79
Isaiah Witzke <isaiah.witzke@gmail.com>
2025-05-18 13:49:34 -0400

    buffer tabs

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require"bufferline".setup()
+    vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>', { desc = 'Next buffer' })
+    vim.keymap.set('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { desc = 'Previous buffer' })
+  end,
+}
